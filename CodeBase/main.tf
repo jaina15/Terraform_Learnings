1db5d82d2356c76a12a4daddddec71bd696f7950
unknown <shubhamjain151097@gmail.com>
2023-06-02 16:38:18 +0530

    updated

--- CodeBase/main.tf
+++ CodeBase/main.tf
@@ -1,51 +1,42 @@
-provider "aws"{
-    region = ""
-    access_key = ""
-    secret_key = ""
+provider "aws" {
+    region = "eu-west-3"
 }
 
-variable "subnet_cidr_block" {
-    description = "subnet cidr block"
-}
+variable vpc_cidr_block {}
 
-variable "vpc_cidr_block" {
-    description = "vpc cidr block"
-}
+variable subnet_cidr_block {}
 
-resource "aws_vpc" "development-vpc" {
+variable avail_zone {}
+
+variable env_prefix {}
+
+resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
-    tags = {
-        Name: "development",
-        vpc_env: "dev"
-    }
+    tags = {Name: "${var.env_prefix}-vpc"}
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp_vpc.id
     cidr_block = var.subnet_cidr_block
-    availability_zone = "us-west-3a"
-    tags = {
-        Name: "dev-subnet-1"
+    availability_zone = var.avail_zone
+    tags = {Name: "${var.env_prefix}-subnet-1"}
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {Name: "${var.env_prefix}-igw"}
+}
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
     }
+    tags = {Name: "${var.env_prefix}-route-table-1"}
 }
 
-data "aws_vpc" "existing-vpc" {
-    default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-    vpc_id = data.aws_vpc.existing-vpc.id
-    cidr_block = "172.31.48.0/20"
-    availability_zone = "us-west-3a"
-    tags = {
-        Name: "dev-subnet-2"
-    }
-}
-
-output "dev-vpc-id" {
-    value = aws_vpc.development-vpc.id
-}
-
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-2.id
+resource "aws_route_table_association" "a-rtb-subnet" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
 }
